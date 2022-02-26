42b3efa589319ae9292b6bc20ab0d2b5fd6293ae
Sachin Patel <85048630+sachin1401@users.noreply.github.com>
2022-02-26 17:29:41 +0530

    Add files via upload

---
+++ botchat.py
@@ -0,0 +1,40 @@
+import time
+print("you want start chat")
+print("type yes / no:")
+con=input()
+
+while con=="yes":
+        print('jay:hii')
+        time.sleep(2)
+        print('        hey:veer')
+        time.sleep(2)
+        print('jay:how are you')
+        time.sleep(2)
+        print('       fine:veer')
+        time.sleep(1)
+        print('   and you?:veer')
+        time.sleep(2)
+        print('jay:me to bro')
+        time.sleep(1)
+        print('jay:today you are free?')
+        time.sleep(2)
+        print('  not really:veer')
+        time.sleep(1)
+        print('but we can meet tomarrow:veer')
+        time.sleep(2)
+        print('jay:ok then we meet tomarrow')
+        time.sleep(2)
+        print('      ok then:veer')
+        time.sleep(1)
+        print('      see you:veer')
+        time.sleep(2)
+        print('jay:bye')
+        time.sleep(2)
+        print('      bye bro:veer')
+        time.sleep(2)
+
+
+while con=="no":
+    print("invalid input")        
+    break
+
